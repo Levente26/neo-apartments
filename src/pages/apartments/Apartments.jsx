d1d7b25d1a412f2234b37c631bf592597e79e498
Levente26 <madi.levente26@gmail.com>
2022-03-30 16:41:22 +0200

    list of apartments

--- src/pages/apartments/Apartments.jsx
+++ src/pages/apartments/Apartments.jsx
@@ -1,17 +1,18 @@
+import { useEffect } from "react"
 import { useApiCall } from "../../hooks/useApiCall"
+import SingleApartment from "./single-apartment/SingleApartment"
 
 const Apartments = () => {
 
     const { apicall, data } = useApiCall() 
 
-    const getData = () => {
+    useEffect(() => {
         apicall()
-    }
-    console.log(data)
+    }, [])
 
     return (
-        <div>
-            <button onClick={getData}>get data</button>
+        <div className="h-full w-full grid grid-cols-3">
+            {data && data.map(apartment => <SingleApartment apartment={apartment} />)}
         </div>
     )
 }
