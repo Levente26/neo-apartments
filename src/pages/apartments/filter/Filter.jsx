e8b419d06dde80add8bd436c2d81211c189c39f2
Levente26 <madi.levente26@gmail.com>
2022-04-05 20:55:51 +0200

    filter jsx

--- src/pages/apartments/filter/Filter.jsx
+++ src/pages/apartments/filter/Filter.jsx
@@ -1,10 +1,42 @@
-
+import { useState } from "react"
 
 const Filter = () => {
+
+    const [travelPlan, setTravelPlan] = useState("")
+    const [date, setDate] = useState(null)
+    const [rooms, setRooms] = useState(0)
+    console.log(date)
+
     return (
-        <div>
-            hi filter
-        </div>
+        <form className="p-4 mx-12 my-8 grid lg:grid-cols-4 border-gray-400 border rounded-xl">
+            <label className="flex flex-col justify-center items-center mb-4">
+                <span className="md:text-lg font-bold text-green-700">Travel plan</span>
+                <input 
+                    type="text"
+                    value={travelPlan}
+                    onChange={(e) => setTravelPlan(e.target.value)}
+                />
+            </label>
+            <label className="flex flex-col justify-center items-center mb-4">
+                <span className="md:text-lg font-bold text-green-700">Arrival date</span>
+                <input 
+                    type="date"
+                    value={date}
+                    onChange={(e) => setDate(e.target.value)}
+                />
+            </label>
+            <label className="flex flex-col justify-center items-center mb-4">
+                <span className="md:text-lg font-bold text-green-700">Number of rooms</span>
+                <input 
+                    type="number"
+                    value={rooms}
+                    onChange={(e) => setRooms(e.target.value)}
+                />
+            </label>
+            <button className="btn bg-green-500 hover:bg-green-700 hover:text-white ease-in-out duration-300">
+                search
+            </button>
+        </form>
     )
 }
 export default Filter
